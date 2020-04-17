37fbe8d324d72c0c8ec4223fd135555d46602a6c
Daniel Bell <db_win10_login@outlook.com>
2020-04-17 20:43:26 -0300

    Changed to MySQL Original SQLite migrations moved to migrations.sqlite

--- binaryblob_ca/binaryblob_ca/settings.py
+++ binaryblob_ca/binaryblob_ca/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'binaryblob_ca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file':os.path.join(BASE_DIR, "my.cnf")
+        },
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Halifax'
 
 USE_I18N = True
 
