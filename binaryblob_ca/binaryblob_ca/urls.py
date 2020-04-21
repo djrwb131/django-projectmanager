f485dc0ebfff66ad2bce2da14b4dbb6c7a7486d3
Daniel Bell <db_win10_login@outlook.com>
2020-04-21 11:44:33 -0300

    Changed to use the default Django admin login (nicer looking, but says "admin" at the top)

--- binaryblob_ca/binaryblob_ca/urls.py
+++ binaryblob_ca/binaryblob_ca/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("project_manager.urls")),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
     path('accounts/', include("django.contrib.auth.urls")),
 ]
