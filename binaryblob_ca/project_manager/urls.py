abb17a46ad10e14c0b01f67e81f286f5b49a8328
Daniel Bell <db_win10_login@outlook.com>
2020-04-22 17:01:48 -0300

    Adjusted urls to make use of django generic views

--- binaryblob_ca/project_manager/urls.py
+++ binaryblob_ca/project_manager/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = "project_manager"
 urlpatterns = (
     path('', views.IndexView.as_view(), name="index"),
-    path('update_status/<int:ipk>/', views.update_task_status, name='update_status'),
-    path('rollback_status/<int:ipk>/', views.rollback_task_status, name='rollback_status'),
+    path('update_status/<int:pk>/', views.update_task_status, name='update_status'),
+    path('rollback_status/<int:pk>/', views.rollback_task_status, name='rollback_status'),
     path('add_task/', views.AddTaskView.as_view(), name="add_task"),
-    path('add_task/submit/', views.add_task_submit, name="add_task_submit"),
-    path('details/<int:ipk>/', views.TaskDetailsView.as_view(), name="details"),
-    path('edit_task/<int:ipk>/', views.EditTaskView.as_view(), name="edit_task"),
+    path('add_task/submit/', views.AddTaskView.as_view(), name="add_task_submit"),
+    path('details/<int:pk>/', views.TaskDetailsView.as_view(), name="details"),
+    path('edit_task/<int:pk>/', views.EditTaskView.as_view(), name="edit_task"),
     path('edit_task/submit/<int:ipk>', views.edit_task_submit, name="edit_task_submit"),
     path('add_note/submit/<int:ipk>', views.add_note_submit, name="add_note"),
 )
