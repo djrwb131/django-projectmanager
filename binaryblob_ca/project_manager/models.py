9965fc8e66d5fe1862c5371cd38bfb4aa9d0f727
Daniel Bell <db_win10_login@outlook.com>
2020-04-22 11:30:21 -0300

    - Fixed exception on empty database - Moved algorithms from views.py - Added basic permissions system

--- binaryblob_ca/project_manager/models.py
+++ binaryblob_ca/project_manager/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from enum import Enum
+
+from django.db.models import F
 
 
 class StatusModel(models.Model):
@@ -10,10 +13,18 @@ class StatusModel(models.Model):
         return self.title
 
 
-# TODO: These classes are pretty devoid of member functions. A lot of stuff is in views.py that shouldn't be.
+# TODO: This seems like an ok way to deal with the default issue. Is there a better way though?
+try:
+    STATUS_DEFAULT = StatusModel.objects.get(progress_id=0)
+except StatusModel.DoesNotExist:
+    a = StatusModel()
+    a.title = "Default!!"
+    a.progress_id = 0
+    a.save()
+
 
 class TaskModel(models.Model):
-    status = models.ForeignKey(StatusModel, on_delete=models.PROTECT, default=StatusModel.objects.get(progress_id=0).pk)
+    status = models.ForeignKey(StatusModel, on_delete=models.PROTECT, default=STATUS_DEFAULT)
     title = models.CharField(max_length=80)
     desc = models.TextField()
     priority = models.IntegerField()
@@ -24,8 +35,95 @@ class TaskModel(models.Model):
     owner = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     parent_task = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
 
+    def _retrieve_tasks_by_nearest_deadline(self):
+        return self.objects.filter(status__progress_id__lt=80).order_by(
+            F('deadline').asc(nulls_last=True)
+        )
+
+    def _retrieve_tasks_by_highest_priority(self):
+        return self.objects.filter(status__progress_id__lt=80).order_by(
+            'priority',
+            F('deadline').asc(nulls_last=True)
+        )
+
+    def _retrieve_tasks_that_need_polish(self):
+        return self.objects.filter(status__progress_id__exact=80).order_by(
+            'priority'
+        )
+
+    def get_most_pressing(self):
+        ret = self._retrieve_tasks_by_nearest_deadline()
+        if len(ret) < 1:
+            ret = self._retrieve_tasks_that_need_polish()
+        else:
+            return ret[0]
+        if len(ret) < 2:
+            return None
+        else:
+            return ret[1]
+
+    def get_highest_priority(self):
+        ret = self._retrieve_tasks_by_highest_priority()
+        if len(ret) < 1:
+            ret = self._retrieve_tasks_that_need_polish()
+        else:
+            return ret[0]
+        if len(ret) < 3:
+            return None
+        else:
+            return ret[2]
+
+    def get_needs_polish(self):
+        ret = self._retrieve_tasks_that_need_polish()
+        if len(ret) < 1:
+            ret = self._retrieve_tasks_by_nearest_deadline()
+        else:
+            return ret[0]
+        if len(ret) < 3:
+            return None  # Nice. Have a KitKat.
+        return ret[2]
+
+    def get_incomplete_tasks(self):
+        return self.objects.filter(
+            status__progress_id__lt=100
+        ).order_by(F('deadline').asc(nulls_last=True))
+
+    def get_complete_tasks(self):
+        return self.objects.filter(
+            status__progress_id__exact=100
+        ).order_by('priority')
+
     def __str__(self):
         return self.title
+
+
+class Permissions(Enum):
+    VIEW = 1
+    EDIT = 2
+    CREATE = 4
+    DELETE = 8
+
+
+# TODO: THIS NEEDS TO BE PROTECTED!
+class TaskPermissionModel(models.Model):
+    task = models.ForeignKey(TaskModel, on_delete=models.CASCADE, related_name="user_permissions")
+    perms = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="task_permissions")
+
+    def has_permission(self, perm):
+        if perm not in Permissions:
+            raise ValueError("Permission with id %i does not exist!" % (perm))
+        return self.perms & perm > 0
+
+    def add_permission(self, perm):
+        if perm not in Permissions:
+            raise ValueError("Permission with id %i does not exist!" % (perm))
+        self.perms = self.perms | perm
+
+    def del_permission(self, perm):
+        if perm not in Permissions:
+            raise ValueError("Permission with id %i does not exist!" % (perm))
+        self.perms = self.perms ^ perm
 
 
 class ChecklistModel(models.Model):
