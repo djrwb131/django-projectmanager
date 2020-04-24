f52f18b6dfdccee845fa922a1cdc7ce05507718a
Daniel Bell <db_win10_login@outlook.com>
2020-04-24 12:44:12 -0300

    - Added colors to the login page - Enabled custom login page - Using a separate base.html; might want to use the same one for everything

--- binaryblob_ca/binaryblob_ca/urls.py
+++ binaryblob_ca/binaryblob_ca/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("project_manager.urls")),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='login'),
     path('accounts/', include("django.contrib.auth.urls")),
 ]
