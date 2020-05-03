6af3a2c67af5d65c7208c33fa378acf1a360945a
Daniel Bell <db_win10_login@outlook.com>
2020-05-03 12:46:28 -0300

    Forgot on_delete; set it all to PROTECT.

--- binaryblob_ca/blog/models.py
+++ binaryblob_ca/blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class BlogEntryModel(models.Model):
-    user = models.ForeignKey(User, null=False)
+    user = models.ForeignKey(User, null=False, on_delete=models.PROTECT)
     created_on = models.DateTimeField(auto_now_add=True, null=False)
     last_edited = models.DateTimeField(auto_now=True, null=False)
     title = models.CharField(max_length=80)
@@ -11,27 +11,27 @@ class BlogEntryModel(models.Model):
 
 
 class CommentModel(models.Model):
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.PROTECT)
     created_on = models.DateTimeField(auto_now_add=True, null=False)
     last_edited = models.DateTimeField(auto_now=True, null=False)
     body = models.TextField(max_length=4096)
 
 
 class BlogCommentModel(models.Model):
-    blog = models.ForeignKey(BlogEntryModel, null=False)
-    comment = models.ForeignKey(CommentModel, null=False)
+    blog = models.ForeignKey(BlogEntryModel, on_delete=models.PROTECT, null=False)
+    comment = models.ForeignKey(CommentModel, on_delete=models.PROTECT, null=False)
 
 
 class BlogEditModel(models.Model):
-    blog_entry_edited = models.ForeignKey(BlogEntryModel, null=False)
-    edit_made_by = models.ForeignKey(User, null=False)
+    blog_entry_edited = models.ForeignKey(BlogEntryModel, on_delete=models.PROTECT, null=False)
+    edit_made_by = models.ForeignKey(User, on_delete=models.PROTECT, null=False)
     date_changed = models.DateTimeField(auto_now_add=True, null=False)
     old_title = models.CharField(max_length=80, null=False, blank=True)
     old_body = models.TextField(max_length=65536)
 
 
 class CommentEditModel(models.Model):
-    comment_edited = models.ForeignKey(CommentModel, null=False)
-    edit_made_by = models.ForeignKey(User, null=False)
+    comment_edited = models.ForeignKey(CommentModel, on_delete=models.PROTECT, null=False)
+    edit_made_by = models.ForeignKey(User, on_delete=models.PROTECT, null=False)
     date_changed = models.DateTimeField(auto_now_add=True, null=False)
     old_comment = models.TextField(max_length=4096)
