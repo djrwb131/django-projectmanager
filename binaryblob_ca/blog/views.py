ad2ae12ae3ae65544991123d58eeba80e67a7928
Daniel Bell <db_win10_login@outlook.com>
2020-05-05 15:35:19 -0300

    - Added pagination

--- binaryblob_ca/blog/views.py
+++ binaryblob_ca/blog/views.py
@@ -15,14 +15,17 @@ from .models import BlogEntryModel, CommentModel
 
 class IndexView(generic.ListView):
     template_name = "blog/index.html"
+    paginate_by = 10
 
     def __init__(self):
         super().__init__()
         self.user = None
+        self.page = 1
 
     def setup(self, request, *args, **kwargs):
         super().setup(request, *args, **kwargs)
         self.user = User.objects.get(pk=kwargs['pk'])
+        self.page = int(request.GET.get('page', 1))
 
     def get_queryset(self, **kwargs):
         if not self.queryset:
@@ -32,6 +35,15 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx['blog_owner'] = self.user
+        max_page = ctx['page_obj'].paginator.num_pages+1
+        page_range = min(max_page, 7)
+        if self.page < 4:
+            ctx['nav_page_range'] = list(range(1,page_range+1))
+        elif self.page > max_page - 4:
+            ctx['nav_page_range'] = list(range(max(1,max_page-page_range), max_page))
+        else:
+            ctx['nav_page_range'] = list(range(max(1, self.page-3), min(self.page+4, max_page)))
+        ctx['menu_items'] = [{'href': reverse('blog:index', args=[self.user.pk]), 'text': 'Index'}]
         return ctx
 
 
