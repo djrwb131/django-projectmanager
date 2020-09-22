3c6c85344d1ceb231d985f6b335f71cdbc8e0928
Daniel Bell <djrwb131@gmail.com>
2020-09-22 18:56:28 -0300

    Removed non-API Views

--- binaryblob_ca/project_manager/views.py
+++ binaryblob_ca/project_manager/views.py
@@ -10,75 +10,6 @@ from .models import TaskModel, TaskNoteModel, ChecklistModel
 from .view_funcs import log_new_task, log_changed_fields
 
 
-class RootView(LoginRequiredMixin, generic.TemplateView):
-    template_name = "project_manager/root_index.html"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.user = None
-
-    def setup(self, request, *args, **kwargs):
-        self.user = request.user
-        return super().setup(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx['checklist'] = ChecklistModel.objects.filter(Q(owner=self.user) | Q(owner=None))
-        qs = TaskModel.objects.filter(Q(owner=self.user) | Q(owner=None))
-        ctx['incomplete_tasks'] = TaskModel.get_incomplete_tasks(qs)
-        return ctx
-
-
-class ChecklistView(LoginRequiredMixin, generic.TemplateView):
-    template_name = "project_manager/widgets/checklist_widget.html"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.user = None
-
-    def setup(self, request, *args, **kwargs):
-        self.user = request.user
-        return super().setup(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        ctx['checklist'] = ChecklistModel.objects.filter(Q(owner=self.user) | Q(owner=None)).order_by("order")
-        qs = TaskModel.objects.filter(Q(owner=self.user) | Q(owner=None))
-        ctx['incomplete_tasks'] = TaskModel.get_incomplete_tasks(qs)
-        return ctx
-
-
-class IndexView(LoginRequiredMixin, generic.ListView):
-    template_name = "project_manager/index.html"
-    model = TaskModel
-    allow_empty = True
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.user = None
-
-    def setup(self, request, *args, **kwargs):
-        self.user = request.user
-        return super().setup(request, *args, **kwargs)
-
-    def get_queryset(self):
-        if not self.queryset:
-            self.queryset = self.model.objects.filter(Q(owner=self.user) | Q(owner=None))
-        return self.queryset
-
-    def get_context_data(self, **kwargs):
-        qs = self.get_queryset()
-        ctx = super().get_context_data(**kwargs)
-
-        ctx['most_pressing'] = self.model.get_most_pressing(qs)
-        ctx['highest_priority'] = self.model.get_highest_priority(qs)
-        ctx['needs_polish'] = self.model.get_needs_polish(qs)
-        ctx['incomplete_tasks'] = self.model.get_incomplete_tasks(qs)
-        ctx['complete_tasks'] = self.model.get_complete_tasks(qs)
-
-        return ctx
-
-
 class TaskDetailsView(PermissionRequiredMixin, generic.DetailView):
     permission_required = "project_manager.view_taskmodel"
     model = TaskModel
@@ -148,4 +79,3 @@ class EditTaskView(PermissionRequiredMixin, generic.edit.UpdateView):
         ctx['edit_mode'] = "edit"
         ctx['notes'] = self.notes_model.objects.filter(task=self.object)
         return ctx
-
